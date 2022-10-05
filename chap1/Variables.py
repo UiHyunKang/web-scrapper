1f600254022353b6b39087ceaddf1a79b9ebec90
ggobo <wjaxkald@naver.com>
2022-10-05 18:53:42 +0900

    booleans
    참과 거짓을 나타낸다
    조건문에 사용된다

--- chap1/Variables.py
+++ chap1/Variables.py
@@ -1,6 +1,11 @@
-a = 2
-b = 3
+a = 2 #2라는 숫자에 a라는 variables(이름)을 만들어 줌
+b = 3 #위와 동일
 c = a + b 
 print(c)
 
-my_age = 25
+my_age = 25 #위와 동일하게 my_age 라는 이름을 만들어주고 25라는 데이터를 저장해 줌
+print(my_age) 
+
+my_name = 1 #중요함 위에 숫자들은 ""를 넣지 않았는데 여기서 넣은 이유는
+                    #Ui Hyun 이라는 단어를 '문자'로 인식시키기 위함임
+print(my_name)
