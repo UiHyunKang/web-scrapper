74375c065fc4d641985bf2b732c3fc1b96a4b350
ggobo <wjaxkald@naver.com>
2022-10-06 13:08:43 +0900

    Create functions.py

---
+++ chap1/functions.py
@@ -0,0 +1,5 @@
+def ssa():#functions란 한번 작성하면 계속 쓸수 있음 print도 하나의 function임
+    print("hello")
+#def ssa(): 로 ssa라는 function을 만듬
+#2칸공백 or tap으로 def 안에 포함 ssa라는 function은 hello라는걸 출력되게 설정함
+ssa() #function을 시작(출력)하는 방법이다 ()가 뒤에 붙어야 한다
