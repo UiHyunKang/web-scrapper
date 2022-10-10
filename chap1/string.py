4c74119b9a6bfb6d75aab629ba85390d306643f7
ggobo <wjaxkald@naver.com>
2022-10-10 11:58:25 +0900

    string

---
+++ chap1/string.py
@@ -0,0 +1,8 @@
+my_name = "Hyun"
+my_age = 25
+my_eye = "black"
+
+print(f"hello im {my_name} {my_age} years, {my_eye} is my eye color ")
+#변수를 포함한 string 만든는 방법이다 제일앞에 f를 넣고 변수는 중괄호 안에 포함 시켜야 한다
+print("hello im {my_name} {my_age} years, {my_eye} is my eye color ")
+#f를 넣지 않으면 변수도 문자로 인식하고 한방에 출력이 된다
