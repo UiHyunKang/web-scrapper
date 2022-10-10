7d12cdfc4a13658fc20b4658b9843fdc97590551
ggobo <wjaxkald@naver.com>
2022-10-10 23:42:54 +0900

    if,elif,else

---
+++ chap2/if.py
@@ -0,0 +1,6 @@
+a = 8
+
+if 10 < a:
+    print("True")
+else:
+    print("False")
---
+++ chap2/if2.py
@@ -0,0 +1,8 @@
+winner = 10
+
+if winner > 10: #winner가 10보다 크면 아래 내용을 출력함
+    print("winner is greater than 10")
+elif winner < 10: # if가 true가 아니고 10보다 작을시 아래 내용을 출력함
+    print("winner is less than 10")
+else: # 위의 조건들이 부합하면 아래 내용이 출력됨
+    print("winner is 10")
