e701072242f17248ef60505cb1d614f19c2144cb
ggobo <wjaxkald@naver.com>
2022-10-14 23:48:53 +0900

    while

---
+++ chap2/while.py
@@ -0,0 +1,5 @@
+distance = 0
+
+while distance < 20: #while은 반복문인데 if와는 달리 계~~~속 돌아감 멈추기 전까지
+    print("i'm running:", distance, "km" )
+    distance = distance + 1 #20보다 낮으면 위 문구를 출력하게 만들었는데 +1씩 함으로 19가 되면 스스로 멈추게 만들었음
