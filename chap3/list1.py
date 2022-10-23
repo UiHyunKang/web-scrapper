680786aa399e16ff4032f70222cf19a643a3b283
ggobo <wjaxkald@naver.com>
2022-10-23 23:58:21 +0900

    Create list1.py

---
+++ chap3/list1.py
@@ -0,0 +1,13 @@
+days = ["Mon", "Tue", "Wed", "Thur", "Fri"] 
+
+print(days[2])
+
+
+
+#days.append("Sun")
+#print(days)
+
+#1 list는 [] 안에 작성하면 된다 각각 ""안에 입력해주고 ,까지 넣어줘야 한다
+#3 수요알을 부르고 싶어서 입력한 값이다
+#3 print[3] 이 아닌 2인 이유는 컴퓨터는 시작이 0이다  만약 friday를 출력하고 싶으면 4를 입력하면 된다
+#아래 days.append("Sun")은 앞에 적은 메소드를 추가한건데 위의 리스트에 sun을 추가한 것이다
