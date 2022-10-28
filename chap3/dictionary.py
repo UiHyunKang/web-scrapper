178d1194e3133d90dbef7b177faf16da22195d1b
ggobo <79427281+UiHyunKang@users.noreply.github.com>
2022-10-28 18:51:18 +0900

    Create dictionary.py

---
+++ chap3/dictionary.py
@@ -0,0 +1,10 @@
+rist = {# dictionary 이름을 rist라고 썼으며 rist라는 사전을 만들었다
+  'name' : 'Hyun',
+  'age' : 25,
+  'alive' : True
+#해당 rist 사전에는 name, age, alive 의 정의를 작성하였다
+}
+
+print(rist)
+rist.pop('age') #rist에서 메소드를 이용해 age 를 뺐다
+print(rist)
